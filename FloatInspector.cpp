5441193c360e94f9f8e7ed7d59eba02f4947171f
Anthony Bongers <bongers.anthony@gmail.com>
2016-05-20 10:35:37 -0400

    Update FloatInspector.cpp

--- FloatInspector.cpp
+++ FloatInspector.cpp
@@ -1,37 +1,23 @@
 #include <iostream>
-#include <string>
-using namespace std;
+#include <bitset>
 
-string createHeader(string title, int bits)
-{
-  int total_width = (bits * 3)      // Each column contains the bit + 2 chars of spacing on each side
-                  + (bits - 1)      // Take into account the pipes separating each column
-                  - title.length(); // The padding needs to factor in the title length
-	                
-  int left_pad  = total_width / 2;
-  int right_pad = total_width - left_pad;
-	
-  return string(left_pad, ' ') + title + string(right_pad, ' ');
-}
+using namespace std;
 
 int main() 
 {
   float f = 1.1f;    // Change this float to see it's underlying bits!
   int i = *(int*)&f; // Reinterpret the bits of the float into an int
-	
-  cout << '|' << createHeader("+", 1)
-       << '|' << createHeader("exponent", 8)
-       << '|' << createHeader("mantissa", 23)
-       << "|\n| ";
-	
-  for (int bit = 31; bit >= 0; --bit)
-  {
-    cout << ((i >> bit) & 1) << " | ";
-  }
+  
+  bitset<1>  sign     { (i >> 31) & 0x00000001 };
+  bitset<8>  exponent { (i >> 23) & 0x000000FF };
+  bitset<23> mantissa { (i      ) & 0x007FFFFF };
+  
+  cout << "| + | exponent |        mantissa         |" << endl;
+  cout << "| " << sign << " | " << exponent << " | " << mantissa << " |";
 }
 
 // Example Output:
 // Input: f = 1.1f;
 //
-// | + |           exponent            |                                         mantissa                                          |
-// | 0 | 0 | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 0 | 0 | 0 | 1 | 1 | 0 | 0 | 1 | 1 | 0 | 0 | 1 | 1 | 0 | 0 | 1 | 1 | 0 | 0 | 1 | 1 | 0 | 1 |
+// | + | exponent |        mantissa         |
+// | 0 | 01111111 | 00011001100110011001101 |
