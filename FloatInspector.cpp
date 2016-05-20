e969399f46225ea1c19bc9687b60ec77f8b9170b
Anthony Bongers <bongers.anthony@gmail.com>
2016-05-20 13:58:47 -0400

    Update FloatInspector.cpp

--- FloatInspector.cpp
+++ FloatInspector.cpp
@@ -3,16 +3,17 @@
 
 int main() 
 {
-  float f = 1.1f;    // Change this float to see the underlying bits!
-  int i = *(int*)&f; // Reinterpret the bits of the float into an int
+  // Change this float to see the underlying bits!
+  float f = 1.1f;
+  int i = *(int*)&f;
   
   std::bitset<1>  sign     { (i >> 31) & 0x00000001 };
   std::bitset<8>  exponent { (i >> 23) & 0x000000FF };
   std::bitset<23> mantissa { (i      ) & 0x007FFFFF };
   
-  std::cout << "Input: " << f << '\n';
-  std::cout << "| + | exponent |        mantissa         |\n";
-  std::cout << "| " << sign << " | " << exponent << " | " << mantissa << " |";
+  std::cout << "Input: " << f << '\n'
+            << "| + | exponent |        mantissa         |\n"
+            << "| " << sign << " | " << exponent << " | " << mantissa << " |";
 }
 
 // Example Output:
